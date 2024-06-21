a7012377d9a0a4cff3016578b7a02dc0c4f2258c
rickysempio <rickysempio@gmail.com>
2024-06-21 20:52:38 +0800

    dom manipulation

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,9 @@
 document.querySelector("#lastModified").innerHTML = `Last Modification: ${ document.lastModified}`;
 
+const hamButton = document.querySelector('nav');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
