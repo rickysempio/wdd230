196e4f94c66ff7d758c7ef7da475121773a02e23
rickysempio <rickysempio@gmail.com>
2024-07-06 23:13:33 +0800

    chamber join page

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,1 +1,28 @@
 document.querySelector("#lastModified").innerHTML = `Last Modification: ${ document.lastModified}`;
+
+
+
+document.getElementById("submitButton").addEventListener("click", function() {
+    var submitButton = document.getElementById("submitButton");
+    submitButton.value = "Submit clicked at " + new Date().toLocaleString(); 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
